c6bbd65bc382bbc58a5cb94bf2df01759df7a173
Hirushika Wijerathne <hirushikawijerathne@gmail.com>
2025-08-25 16:41:06 +0530

    second commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,8 +5,10 @@ const connectDB = async () => {
         mongoose.connection.on(`connected`, ()=> console.log("Databse Connected")
     );
         await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+        console.log("MongoDB connected");
     } catch (error) {
-        console.log(error.message);  
+        console.error(error);
+        process.exit(1);
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors()); // Enable Cross-Origin Resource Sharing
 app.use(express.json())
 app.use(clerkMiddleware())
 
-//API 
+//API
 app.post("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res) => res.send("API is working"));
